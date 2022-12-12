9c381aa7c5c22461bb88234a030876ef277e0365
Revanth Ponna <93521732+revanthponna@users.noreply.github.com>
2022-12-12 13:12:44 +0530

    Update GroundTile.cs

--- Scripts/GroundTile.cs
+++ Scripts/GroundTile.cs
@@ -14,7 +14,7 @@ public class GroundTile : MonoBehaviour
     private void OnTriggerExit(Collider other)
     {
         groundSpawner.SpawnTile(true);
-        Destroy(gameObject, 2);
+        Destroy(gameObject, 2); // Destroying the tile after player has passed to save memory
     }
     // Update is called once per frame
     void Update()
@@ -37,11 +37,11 @@ public class GroundTile : MonoBehaviour
 
     public void SpawnCoins()
     {
-        int coinsToSpawn = 10;
+        int coinsToSpawn = 10; // spawning ten coins in one ground tile
         for (int i = 0; i < coinsToSpawn; i++)
         {
             GameObject temp = Instantiate(coinPrefab, transform);
-            temp.transform.position = GetRandomPointInCollider(GetComponent<Collider>());
+            temp.transform.position = GetRandomPointInCollider(GetComponent<Collider>()); // getting the random point to spawn coin
         }
     }
 
@@ -51,10 +51,10 @@ public class GroundTile : MonoBehaviour
             Random.Range(collider.bounds.min.x, collider.bounds.max.x),
             Random.Range(collider.bounds.min.y, collider.bounds.max.y),
             Random.Range(collider.bounds.min.z, collider.bounds.max.z)
-            );
+            ); // choosing a random point to spawn coin
         if (point != collider.ClosestPoint(point))
         {
-            point = GetRandomPointInCollider(collider);
+            point = GetRandomPointInCollider(collider); // making sure two coins don't spawn at the same point
         }
 
         point.y = 1;
